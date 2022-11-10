91dd0bfc5446bb540f74aba016ad8dd1a66b1981
jkpatil123 <100862591+jkpatil123@users.noreply.github.com>
2022-11-10 12:26:47 +0530

    update 2 project.js

--- project.js
+++ project.js
@@ -62,7 +62,7 @@ function addTransactionDOM(transaction) {
   item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
 
   item.innerHTML = `
-    ₹{transaction.text} <span>₹{sign}${Math.abs(
+    ₹{transaction.text} <span>₹{sign}₹{Math.abs(
     transaction.amount
   )}</span> <button class="delete-btn" onclick="removeTransaction(${
     transaction.id
