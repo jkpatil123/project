b6c894b80aa0570a775cc13fdbed0fe3083981e2
jkpatil123 <100862591+jkpatil123@users.noreply.github.com>
2022-11-10 12:03:15 +0530

    update oroject.js

--- project.js
+++ project.js
@@ -88,9 +88,9 @@ function updateValues() {
     -1
   ).toFixed(2);
 
-  balance.innerText = `$${total}`;
-  money_plus.innerText = `$${income}`;
-  money_minus.innerText = `$${expense}`;
+  balance.innerText = `₹₹{total}`;
+  money_plus.innerText = `₹₹{income}`;
+  money_minus.innerText = `₹₹{expense}`;
 }
 
 // Remove transaction by ID
